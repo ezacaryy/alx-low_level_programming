33f4fe847c47972c7bedd7dff69b25b94aeb0dc7
ezacary <zacaria.cheikh@gmail.com>
2023-11-23 12:20:59 -0800

    0x08. C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,11 +11,13 @@ void _puts_recursion(char *s)
     if (*s == '\0')
     {
         _putchar('\n');
+	return;
     }
     else
     {
         _putchar(*s);
-        _puts_recursion(s + 1);
+	s++;
+        _puts_recursion(s);
     }
 }
 
