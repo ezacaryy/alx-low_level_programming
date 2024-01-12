9c83d28012579ab5ec912de01efa7bdf5fbeeb97
Zakaria Cheikh <133505628+ezacaryy@users.noreply.github.com>
2024-01-12 02:02:28 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include "main.h"
+include "main.h"
 /**
  * _puts_recursion - function like puts();
  * @s: input
@@ -8,12 +8,12 @@ void _puts_recursion(char *s)
 {
         if (*s == '\0')
         {
-                _putchar('\0');
+                _putchar('\n');
         }
 
         else
         {
-                _putchar(*s');
+                _putchar(*s);
                 _puts_recursion(s + 1);
         }
 }
