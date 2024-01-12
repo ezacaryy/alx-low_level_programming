2371612370d179292135dbc5b67716ddc4d51c3c
Zakaria Cheikh <zedd@Zakarias-MacBook-Air.local>
2024-01-12 02:07:50 +0100

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-        if (*s == '\0')
+        if (*s != '\0')
         {
-                _putchar('\0');
+                _putchar(*s);
+                _puts_recursion(s + 1);
         }
-
         else
         {
-                _putchar(*s');
-                _puts_recursion(s + 1);
+                _putchar('\n');
         }
 }
