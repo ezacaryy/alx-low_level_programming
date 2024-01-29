ce6b7c5da862b564f695970fa8858b8d2c985652
Zakaria Cheikh <133505628+ezacaryy@users.noreply.github.com>
2024-01-29 18:02:32 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 #include "holberton.h"
 
-=======
- 0x08-recursion/0-puts_recursion.c | 18 ++++++++++--------
-#include "main.h"
->>>>>>> c4becfbc7496c28b1c916cfedd085cbc703caf11
 /**
  * _puts_recursion - Prints a string followed by a new line
  * @s: string
@@ -14,7 +9,6 @@
  */
 void _puts_recursion(char *s)
 {
-<<<<<<< HEAD
 	if (*s == 0)
 	{
 		_putchar('\n');
@@ -22,16 +16,4 @@ void _puts_recursion(char *s)
 	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
-}}
-=======
-        if (*s != '\0')
-        {
-                _putchar(*s);
-                _puts_recursion(s + 1);
-        }
-        else
-        {
-                _putchar('\n');
-        }
 }
->>>>>>> c4becfbc7496c28b1c916cfedd085cbc703caf11
